e06fae3fc4aae58b193a01d68e1fcf094f9d6372
ag171980 <ag171980@gmail.com>
2022-10-05 18:34:17 -0300

    c

--- src/pages/auth/CrearCuenta.js
+++ src/pages/auth/CrearCuenta.js
@@ -25,7 +25,6 @@ export default function CrearCuenta() {
             method: 'POST',
             headers: {
                 'Access-Control-Allow-Origin': '*',
-                'origin': 'x-requested-with',
                 'Access-Control-Allow-Headers': 'POST, GET, PUT, DELETE, OPTIONS, HEAD, Authorization, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin',
                 'Content-Type': 'application/json',
             },
