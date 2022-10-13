2a9bdf1c83d29c00d3e5caf923caa75db0129755
FranciscoSantos01 <franciscosantosaraoz2000@gmail.com>
2022-10-12 21:57:45 -0300

    Verificacion log in

--- src/pages/auth/CrearCuenta.js
+++ src/pages/auth/CrearCuenta.js
@@ -1,8 +1,18 @@
 import axios from 'axios'
 import React from 'react'
 import Navbar from '../components/NavBar/navbar'
+import { useState } from 'react';
+import { Formik, Field, Form} from 'formik';
 import { Link } from 'react-router-dom'
+import * as yup from 'yup'
 
+const reviewSchema = yup.object({
+    email: yup.string().email('Invalid Email').required('required'),
+    nombre: yup.string().required().min(4).max(10),
+    apellido: yup.string().required().min(4).max(10),
+    contraseña: yup.string().required().matches(/^[0-9A-Za-z]*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?][0-9a-zA-Z]*$/,
+        'Need one special character',).min(8).max(15),
+})
 export default function CrearCuenta() {
     function showPwd() {
         if (document.querySelector('#pwd_user').type === "password") {
@@ -46,6 +56,8 @@ export default function CrearCuenta() {
         // }
 
     }
+  
+   
     return (
         <React.Fragment>
             <div className="flex layout">
@@ -59,39 +71,83 @@ export default function CrearCuenta() {
                                     <h3 className="highlights layout">¿Ya tenés cuenta?  <Link to="/login" className="crea_cuenta"> Iniciar sesión</Link></h3>
                                     <div>
                                     </div>
-
-                                    <form>
-                                        <div className="mb-3">
-                                            <label for="exampleInputEmail1" className="form-label">Correo electrónico</label>
-                                            <input type="email" className="form-control" id="email_user" aria-describedby="emailHelp" />
-                                        </div>
-                                        <div className=" nombre_completo mb-3">
-                                            <div className='input-cont'>
-                                                <label for="text_nombre" className="form-label">Nombre</label>
-                                                <input type="text" className="form-control" id="text_nombre" aria-describedby="emailHelp" />
-                                            </div>
-
-                                            <div className='input-cont'>
-                                                <label for="text_apellido" className="form-label">Apellido</label>
-                                                <input type="text" className="form-control" id="text_apellido" aria-describedby="emailHelp" />
-                                            </div>
-                                        </div>
-                                        <div className="cont-pwd mb-3">
-                                            <label for="exampleInputPassword1" className="form-label">Contraseña</label>
-                                            <input type="password" className="form-control" name="pwd_user" id="pwd_user" />
-                                            <svg onClick={showPwd} className='eye_hide bi bi-eye-slash-fill' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                                                <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z" />
-                                                <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12-.708.708z" />
-                                            </svg>
-                                        </div>
-
-                                        <div className="block10 layout">
-
-                                            <button type="submit" className="btn btn-outline-danger espacio" onClick={crearCuenta}>Crear cuenta</button>
-
-                                            <button type="button" className="btn btn-outline-secondary espacio"><img src="../assets/img/44d99b5c0ac21f42e4480177f374344a.png" alt="" width="20" />Ingresar con Google</button>
-                                        </div>
-                                    </form>
+                                    <Formik
+    initialValues={{email:'', nombre:'', apellido:'', contraseña:''}}
+    validationSchema={reviewSchema}
+    onSubmit={(values,actions,) => {
+        actions.resetForm();
+        console.log(values);
+      }}
+    >
+   {(props)=>(
+    <Form>
+    <div className="mb-3">
+    <label for="exampleInputEmail1" className="form-label">Correo electrónico</label>
+    <Field id="email_user"
+          name="email"
+          placeholder="jane@acme.com"
+          type="email"
+          className="form-control"
+          onChangeText={props.handleChange('email')}
+          onBlur={props.handleBlur('email')} 
+             value={props.values.email}
+           />
+      <span className='Errorcolor'>{props.touched.email && props.errors.email}</span>     
+    </div>
+    <div className=" nombre_completo mb-3">
+        <div className='input-cont'>
+            <label for="text_nombre" className="form-label">Nombre</label>
+            <Field 
+            id="text_nombre" 
+            name="nombre" 
+            type="text" 
+            className="form-control"
+            onChangeText={props.handleChange('nombre')}
+             onBlur={props.handleBlur('nombre')} 
+             value={props.values.nombre} 
+            />
+            <span className='Errorcolor'>{props.touched.nombre && props.errors.nombre}</span>   
+            </div>
+            <div className='input-cont'>
+            <label for="text_apellido" className="form-label">Apellido</label>
+            <Field 
+            id="text_apellido" 
+            name="apellido" 
+            type="text" 
+            onChangeText={props.handleChange('apellido')}
+             onBlur={props.handleBlur('apellido')} 
+             value={props.values.apellido}
+             className="form-control"
+            />
+            <span className='Errorcolor'>{props.touched.apellido && props.errors.apellido}</span>   
+            </div>
+            </div>
+    <div className="cont-pwd mb-3">
+        <label for="exampleInputPassword1" className="form-label">Contraseña</label>
+        <Field 
+        id="pwd_user" 
+        name="contraseña" 
+        type="password" 
+        className="form-control"
+        secure={true}
+        onChangeText={props.handleChange('contraseña')}
+        onBlur={props.handleBlur('contraseña')} 
+        value={props.values.contraseña}
+        />
+        <svg  className='eye_hide bi bi-eye-slash-fill' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+            <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z" />
+            <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12-.708.708z" />
+         </svg>
+         <span className='Errorcolor'>{props.touched.contraseña && props.errors.contraseña}</span>   
+    </div>
+    <div className="block10 layout">
+        <button type="submit" className="btn btn-outline-danger espacio" onClick={props.handleSubmit}>Crear cuenta</button>
+        <button type="button" className="btn btn-outline-secondary espacio"><img src="../assets/img/44d99b5c0ac21f42e4480177f374344a.png" alt="" width="20" />Ingresar con Google</button>
+    </div>
+    </Form>
+   )}
+    </Formik>
+                                    
                                 </div>
                             </div>
                         </div>
