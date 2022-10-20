de341c2ac1f1da38bc345f27e2d9617d85f89ad1
ag171980 <ag171980@gmail.com>
2022-10-20 11:47:39 -0300

    subo avances en home y recetas

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,65 +5,17 @@ import React, { useEffect, useState } from 'react'
 //componentes
 import Card from '../components/Card/card'
 import Navbar from '../components/NavBar/navbar'
-// import Nav from '../components/NavBar/Nav';
+import Cerrar from '../../assets/img/close.png'
+// import N`av from '../components/NavBar/Nav';
 import "./home.css"
 // import { Context } from '../contexto/Context';
 // import { useContext } from 'react';
 
 export default function Home() {
-    const recetasComida = [
-        {
-            id: 1,
-            img: "pollito.png",
-            tipo: "Carnes",
-            nombre: "Pollo al horno con papas",
-            descripcion: "Pollito al horno con papas riki riki.  Laborum magna nulla duis ullamco cillum dolor. Laborum magna nulla duis ullamco cillum dolor.",
-            duracion: 30,
-            cantidadIngredientes: 6,
-            dificultad: "Fácil"
-        },
-        {
-            id: 2,
-            img: "milanesas.jpg",
-            tipo: "Carnes",
-            nombre: "Milanesas Napolitanas",
-            descripcion: "Milanesita napolitanas que tienen salsa, jamon, queso y tomate bien tanas. Laborum magna nulla duis ullamco cillum dolor.",
-            duracion: 30,
-            cantidadIngredientes: 6,
-            dificultad: "Dificil"
-        },
-        {
-            id: 3,
-            img: "Ensalada-cesar.jpg",
-            tipo: "Vegetales",
-            nombre: "Ensalada Cesar",
-            descripcion: "Ensalada Cesar bien simple para que comas sanito. Laborum magna nulla duis ullamco cillum dolor.",
-            duracion: 50,
-            cantidadIngredientes: 4,
-            dificultad: "Fácil"
-        },
-        {
-            id: 4,
-            img: "Ensalada-frutas.jpg",
-            tipo: "Frutas",
-            nombre: "Ensalada de Frutas",
-            descripcion: "Ensalada de frutas ideal para comer despues de una ensaladita Cesar, facil de hacer. Laborum magna nulla duis ullamco cillum dolor.",
-            duracion: 20,
-            cantidadIngredientes: 3,
-            dificultad: "Fácil"
-        }
-    ]
     const [ingredientes, setIngrediente] = useState([]);
-    let resultadoFinal = []
-    // const { log, account } = useContext(Context)
     const [recetas, setRecetas] = useState([])
-    // function compare(props1, props2) {
-    //     if (props1[0].email !== props2.email && props1[0].contraseña !== props2.contraseña) {
-    //         return false;
-    //     } else {
-    //         return true;
-    //     }
-    // }
+    let stateRecetas = false
+
 
     // function llenarIngredientes(e) {
     //     e.preventDefault();
@@ -84,24 +36,27 @@ export default function Home() {
         try {
             const response = await axios.get('https://kecomer.pythonanywhere.com/recipes/recipes/');
             if (response.status === 200) { // response - object, eg { status: 200, message: 'OK' }
-                // console.log('success stuff');
-                // console.log(response.data)
-
+                stateRecetas = true
                 let contArr = 0;
                 Object.entries(response.data).forEach((el) => {
-                    // console.log(arrTypes[contArr])
-
                     if (el[0] === arrTypes[contArr]) {
-                        el[1].map((rec) =>
-                            setRecetas([...recetas, rec])
-                        )
-                        // setRecetas([...el[1]])
-                        // el[1].map(rec => {
-                        //     console.log(rec)
-                        //     console.log("---")
-                        //     setRecetas([...rec])
-                        // })
+                        el[1].map((rec) => {
+                            setRecetas(recetas => [...recetas, rec])
+                        })
                     }
+
+                    // setRecetas([...recetas, el[1]])
+                    // el[1].map((rec) =>
+                    //     // recetas.push(rec)
+
+                    // )
+                    // setRecetas([...el[1]])
+                    // el[1].map(rec => {
+                    //     console.log(rec)
+                    //     console.log("---")
+                    //     setRecetas([...rec])
+                    // })
+
 
                 });
                 return true;
@@ -113,21 +68,11 @@ export default function Home() {
         }
     }
 
-    // console.log(makeRequest())
-    // axios({
-    //     method: 'GET',
-    //     url: "https://kecomer.pythonanywhere.com:3030/admin/recipes/recipesmodel/",
-    //     responseType: 'stream'
-
-    // })
-    //     .then((data) => {
-    //         console.log(data)
-    //     })
     const mostrarRecetas = async () => {
         await makeRequest(ingredientes)
 
         setTimeout(() => {
-            
+
 
             // document.querySelector(".category").classList.add("hide")
             // document.querySelector(".presentacion").classList.add("hide")
@@ -152,7 +97,7 @@ export default function Home() {
                         ingredientes</span>
                     </p>
                     <div className="category">
-                        <div className="divCategory" onClick={() => llenarIngredientes("C")}>
+                        <div className="divCategory" onClick={() => llenarIngredientes("CARNE")}>
                             <img src="../assets/img/carnes.png" alt='Carnes' />
                             <span>Carnes</span>
                             {/* <span onClick={modal}>Carnes</span> */}
@@ -166,11 +111,11 @@ export default function Home() {
                             <img src="../assets/img/frutas.png" alt='Frutas' />
                             <span>Frutas</span>
                         </div>
-                        <div className="divCategory">
+                        <div className="divCategory" onClick={() => llenarIngredientes("C")}>
                             <img src="../assets/img/lacteos.png" alt='Lacteos' />
                             <span>Lateos</span>
                         </div>
-                        <div className="divCategory">
+                        <div className="divCategory" onClick={() => llenarIngredientes("CO")}>
                             <img src="../assets/img/especias.png" alt='Especias' />
                             <span>Condimentos</span>
                         </div>
@@ -180,7 +125,7 @@ export default function Home() {
                         </div>
                         <div className="divCategory">
                             <img src="../assets/img/grasas.png" alt='Grasas' />
-                            <span>Grasas y aceites</span>
+                            <span>Grasas y aceites</span>--
                         </div>
                         <div className="divCategory">
                             <img src="../assets/img/harinas-y-granos.png" alt='Harinass' />
@@ -198,28 +143,39 @@ export default function Home() {
                         <button onClick={mostrarRecetas} className="btn4">Buscar recetas</button>
                     </div>
                     <div className='ingredientes'>
-                        {ingredientes.map((ingred, index) => {
-                            return <div className='ingredienteItem' key={index}>
-                                <p>{ingred}</p>
-                                <button className='eliminarIngrediente' onClick={() => eliminarIngrediente(ingred)}>x</button>
+                        {ingredientes.map((ingred, index) =>
+                            <div className='ingredienteItem' key={index}>
+                                {ingred == "CARNE" &&
+                                    <p>Carnes</p>
+                                }
+                                {ingred == "V" &&
+                                    <p>Vegetales</p>
+                                }
+                                {ingred == "P" &&
+                                    <p>Frutas</p>
+                                }
+                                {ingred == "PA" &&
+                                    <p>Lacteos</p>
+                                }
+                                {ingred == "PA" &&
+                                    <p>Lacteos</p>
+                                }
+                                {ingred == "CO" &&
+                                    <p>Condimentos</p>
+                                }
+                                <button className='eliminarIngrediente' onClick={() => eliminarIngrediente(ingred)}><img src={Cerrar} /></button>
                             </div>
-                        })}
+                        )}
                     </div>
 
                     <div className='contenedorRecetas'>
                         {
+                            recetas.length != 0 &&
                             recetas.map((receta, index) => <Card recetaInfo={receta} key={index} />)
-                            // resultadoFinal.map((receta) =>
-                            //     <Card recetaInfo={receta} key={receta.id} />
-                            // )
                         }
-                        {/* <Card recetaInfo={}/> */}
-                        {/* <Card />
-                        <Card />
-                        <Card />
-                        <Card />
-                        <Card />
-                        <Card /> */}
+                        {stateRecetas == true &&
+                            <p> A</p>
+                        }
 
                     </div>
                 </main>
